eab35fdac064c1ab403d0eb323d4eb37ba2bf91e
Niels van der Linden <linden.2015.post@gmail.com>
2018-04-21 13:22:08 +0200

    Feature: git blame-git-branches :: nearest tag

--- git/blame-git-branches.bash
+++ git/blame-git-branches.bash
@@ -10,15 +10,16 @@
 # The script has no running options.
 
 REMOTE_BRANCHES=$(git branch --remotes | egrep -v 'origin/(master$|test/|acceptance/|release/)' | awk '{print($1)}')
-PRINT_ROWS='Branch|Author|Updated|Is Merged\n'
+PRINT_ROWS='BRANCH|AUTHOR|UPDATED|IS MERGED|NEAREST TAG\n'
 for i in $REMOTE_BRANCHES; do
+    AUTHOR=$(git log -1 --format=format:'%cn' $i)
+    DATE=$(git log -1 --format=format:'%cr' $i)
     if [ $(git branch --remotes --contains $i | grep origin/master | wc -l) -gt 0 ]; then
         IS_MERGED='Yes'
     else
         IS_MERGED='No'
     fi
-    AUTHOR=$(git log -1 --format=format:'%cn' $i)
-    DATE=$(git log -1 --format=format:'%cr' $i)
-    PRINT_ROWS+="$i|$AUTHOR|$DATE|$IS_MERGED\n"
+    PARENT_TAG=$(git describe --contains $i 2> /dev/null)
+    PRINT_ROWS+="$i|$AUTHOR|$DATE|$IS_MERGED|$PARENT_TAG\n"
 done
 echo -e $PRINT_ROWS | column -t -s '|'
