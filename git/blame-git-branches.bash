4fc814caa8d374732781a350c4af51ec781e1936
Niels van der Linden <linden.2015.post@gmail.com>
2018-04-21 14:13:59 +0200

    Refactor: git blame-git-branches :: remove superfluous 'is merged' because of 'merged into'

--- git/blame-git-branches.bash
+++ git/blame-git-branches.bash
@@ -10,18 +10,16 @@
 # The script has no running options.
 
 REMOTE_BRANCHES=$(git branch --remotes | egrep -v 'origin/(master$|test/|acceptance/|release/)' | awk '{print($1)}')
-PRINT_ROWS='BRANCH|AUTHOR|UPDATED|IS MERGED|MERGED INTO|RECENT ANCESTOR\n'
+PRINT_ROWS='BRANCH|AUTHOR|UPDATED|MERGED INTO|RECENT ANCESTOR\n'
 for i in $REMOTE_BRANCHES; do
     AUTHOR=$(git log -1 --format=format:'%cn' $i)
     DATE=$(git log -1 --format=format:'%cr' $i)
     if [ $(git branch --remotes --contains $i | grep origin/master | wc -l) -gt 0 ]; then
-        IS_MERGED='Yes'
         MERGED_INTO=$(git describe --contains $i 2> /dev/null)
     else
-        IS_MERGED='No'
         MERGED_INTO='N/A'
     fi
     RECENT_ANCESTOR=$(git merge-base origin/master $i | xargs git describe --exact-match 2> /dev/null)
-    PRINT_ROWS+="$i|$AUTHOR|$DATE|$IS_MERGED|$MERGED_INTO|$RECENT_ANCESTOR\n"
+    PRINT_ROWS+="$i|$AUTHOR|$DATE|$MERGED_INTO|$RECENT_ANCESTOR\n"
 done
 echo -e $PRINT_ROWS | column -t -s '|'
